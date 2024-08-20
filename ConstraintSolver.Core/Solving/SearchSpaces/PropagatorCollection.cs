219e4236929ae1847d95b95cb57eb9de4074e912
koent <ktbbb5@gmail.com>
2024-08-20 20:41:03 +0200

    Move PropagatorCollection to SearchSpaces folder

--- ConstraintSolver.Core/Solving/SearchSpaces/PropagatorCollection.cs
+++ ConstraintSolver.Core/Solving/SearchSpaces/PropagatorCollection.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using ConstraintSolver.Core.Solving.Propagators;
 
-namespace ConstraintSolver.Core.Solving.Propagators;
+namespace ConstraintSolver.Core.Solving.SearchSpaces;
 
 public class PropagatorCollection
 {
