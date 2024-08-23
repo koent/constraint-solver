c95be4d317771224a950bd8178f0c24c43e140d8
koent <ktbbb5@gmail.com>
2024-08-23 21:32:11 +0200

    Permutation propagator: fix variable if it is the only one allowing certain value

--- ConstraintSolver.Core/Solving/Propagators/Permutation.cs
+++ ConstraintSolver.Core/Solving/Propagators/Permutation.cs
@@ -20,7 +20,42 @@ public class Permutation(IEnumerable<int> variables) : IPropagator
             return (Status.Failed, []);
         }
 
-        return (Status.AtFixpoint, []);
+        var updatedVariableIndices = new HashSet<int>();
+        var changed = true;
+        while (changed)
+        {
+            changed = false;
+            Dictionary<int, List<int>> valueToVariableIndex = [];
+            foreach (var variableIndex in _variableIndices)
+            {
+                foreach (var value in store.Variables[variableIndex].Domain())
+                {
+                    if (valueToVariableIndex.TryGetValue(value, out List<int> indices))
+                    {
+                        indices.Add(variableIndex);
+                    }
+                    else
+                    {
+                        valueToVariableIndex[value] = [variableIndex];
+                    }
+                }
+            }
+
+            foreach (var (value, variableIndices) in valueToVariableIndex)
+            {
+                if (variableIndices.Count == 1)
+                {
+                    var variableIndex = variableIndices.Single();
+                    if (store.Variables[variableIndex].Update([value]))
+                    {
+                        changed = true;
+                        updatedVariableIndices.Add(variableIndex);
+                    }
+                }
+            }
+        }
+
+        return (Status.AtFixpoint, updatedVariableIndices);
     }
 
     public IEnumerable<int> VariableIndices() => _variableIndices;
